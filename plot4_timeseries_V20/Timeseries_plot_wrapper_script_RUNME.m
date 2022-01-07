e1b9970bacbfc8409bbf899dbb12287f4bd39fc0
Sam Jones <SA01SJ@sams.ac.uk>
2022-01-07 10:40:53 +0000

    Small changes to bring Viking20 plotting up to speed with other plotting routines

--- plot4_timeseries_V20/Timeseries_plot_wrapper_script_RUNME.m
+++ plot4_timeseries_V20/Timeseries_plot_wrapper_script_RUNME.m
@@ -4,7 +4,7 @@ addpath functions
 addpath(genpath('../other_functions'));
 addpath('I:\iAtlantic_overflow\VIKING20\D1p2'); %  addpath('H:/Viking20X'); % <-  data location
 
-regions = 2; % [1:12]; % 12
+regions = 1; % [1:12]; % 12
 for aa= 1:length(regions)
     region = regions(aa); % 2 % 7 % 8
     p4_plot_timeseries_worker(region)
